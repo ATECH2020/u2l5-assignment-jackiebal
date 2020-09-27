1eaf0664acbc23db77da03e48615440575ac6289
Jacqueline Balanovsky <jacqueli.2309858@nv.ccsd.net>
2020-09-27 22:28:46 +0000

    finished How Far Away - have to fix some formatting though

--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,24 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+        Scanner in = new Scanner(System.in);
+        double firstLat, secondLat, firstLong, secondLong;
+
+        System.out.print("Enter the latitude of the starting location: ");
+        firstLat = in.nextDouble();
+       
+        System.out.print("Enter the longitude of the starting location: ");
+        firstLong = in.nextDouble();
+ 
+        System.out.print("Enter the latitude of the ending location: ");
+        secondLat= in.nextDouble();
+
+        System.out.print("Enter the longitude of the ending location: ");
+        secondLong = in.nextDouble();
+        
+        GeoLocation geo1 = new GeoLocation(firstLat, firstLong);
+        GeoLocation geo2 = new GeoLocation(secondLat, secondLong);
+
+        System.out.print("The distance is " + geo1.distanceFrom(geo2) + " miles.");
     }
 }
